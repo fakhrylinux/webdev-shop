9c48f7839972c3b98b133b4efaff713fec8d95bf
Fakhry Linux <fakhry.linux@gmail.com>
2024-06-22 12:49:33 +0800

    remove image and desc table from users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,17 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image');
             $table->string('email')->unique();
-            $table->text('description');
-            $table->integer('price');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
     }
-    /** 
+    /**
      * Reverse the migrations.
      */
     public function down(): void
